221239032138d7f18b3fa1c92b93190ea9abddce
poneilldev <p.oneill.apc@gmail.com>
2014-11-09 02:29:15 -0700

    Initial Commit

---
+++ StrategyPatternDesign/StrategyPatternDesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrategyPatternDesign
+//
+//  Created by Paul O'Neill on 11/9/14.
+//  Copyright (c) 2014 Paul O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
