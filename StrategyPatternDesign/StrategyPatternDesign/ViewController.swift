627c68d780fea9a6f968f644a680d3f552488d19
poneilldev <p.oneill.apc@gmail.com>
2014-11-09 02:30:49 -0700

    Strategy Pattern Design

--- StrategyPatternDesign/StrategyPatternDesign/ViewController.swift
+++ StrategyPatternDesign/StrategyPatternDesign/ViewController.swift
@@ -8,11 +8,54 @@
 
 import UIKit
 
+
+/******************************************************
+* PROTOCOL - it's like a Java Interface
+* It gives certain instructions on what needs to be
+* implemented
+* This example is a little different from the car example
+* I showed, but it still drives the point home
+********************************************************/
+protocol PrintStrategy {
+    func printString(string: String) -> String
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        class Printer {
+            
+            let strategy: PrintStrategy
+            
+            func printString(string: String) -> String {
+                return self.strategy.printString(string)
+            }
+            
+            init(strategy: PrintStrategy) {
+                self.strategy = strategy
+            }
+        }
+        
+        class UpperCaseStrategy : PrintStrategy {
+            func printString(string:String) -> String {
+                return string.uppercaseString
+            }
+        }
+        
+        class LowerCaseStrategy : PrintStrategy {
+            func printString(string:String) -> String {
+                return string.lowercaseString
+            }
+        }
+        
+        var lower = Printer(strategy:LowerCaseStrategy())
+        println(lower.printString("Hello, World"))
+        
+        var upper = Printer(strategy:UpperCaseStrategy())
+        println(upper.printString("Hello, World!"))
     }
 
     override func didReceiveMemoryWarning() {
